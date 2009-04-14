9e44829109a2a45a6154f0d6697ad2bf38a4ad20
Hector E. Gomez Morales <hectoregm@gmail.com>
2009-04-14 15:56:13 -0500

    Minor comment edit.

--- hector/init.el
+++ hector/init.el
@@ -77,7 +77,7 @@
 ;; Cucumber mode
 (load "vendor/cucumber-mode")
 
-;;; csharp-mode site-lisp configuration
+;;; C# mode configuration
 (autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
 (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-mode))
 
