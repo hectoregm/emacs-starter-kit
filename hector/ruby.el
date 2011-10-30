acb64bbe948c8377d1834bf0e358cf251fe4352f
Hector E. Gomez Morales <hectoregm@gmail.com>
2011-10-29 22:58:12 -0500

    Add keybinding to eval a ruby buffer.

--- hector/ruby.el
+++ hector/ruby.el
@@ -1,6 +1,11 @@
 (require 'rvm)
 (rvm-use-default)
+
+(defun ruby-keys ()
+  (local-set-key (kbd "M-r") 'ruby-compilation-this-buffer))
+
 (add-hook 'ruby-mode-hook 'ruby-electric-mode)
 (add-hook 'ruby-mode-hook 'turn-on-whitespace)
+(add-hook 'ruby-mode-hook 'ruby-keys)
 
 (provide 'hector/ruby)
