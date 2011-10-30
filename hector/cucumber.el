dd9685d3ace638a5fdf4084abbfa6d923223b548
Hector E. Gomez Morales <hectoregm@gmail.com>
2011-10-29 23:29:21 -0500

    Use feature-mode from ELPA.

--- hector/cucumber.el
+++ hector/cucumber.el
@@ -1,6 +1,3 @@
-;; Cucumber
-(add-to-list 'load-path (concat dotfiles-dir "vendor/feature-mode"))
-(require 'feature-mode)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
 
 (add-hook 'feature-mode-hook 'run-coding-hook)
